e2275cac6a70e3d7413afebf71be1ce0f79946a6
Axel Belin <64922136+AxelBelin@users.noreply.github.com>
2022-03-27 14:51:12 +0200

    Update Test.java

--- fr.esipe.tp2/Test.java
+++ fr.esipe.tp2/Test.java
@@ -1,10 +1,11 @@
 package fr.esipe.tp2 ;
 public class Test {
-
+	
 	public static void main(String[] args) {
+		
 		var first = args[0];
-        var second = args[1];
-        var last = args[2];
-        System.out.println(first + ' ' + second + ' ' + last) ;
+		var second = args[1];
+		var last = args[2];
+		System.out.println(first + ' ' + second + ' ' + last) ;
 	}
 }
